ac30a5b5a0bdf80703ccb05a4facd2a9790bffe9
Kooperatorko <71215527+Kooperatorko@users.noreply.github.com>
2021-02-14 23:37:56 +0300

    Update task_6.py

--- 09.02.2021/task_6.py
+++ 09.02.2021/task_6.py
@@ -2,10 +2,11 @@ def summ(slov):
     print('Словарь: ', slov)
     return(sum([value for value in slov.values()]))
 
-def proiz(slov, res, k):
+def proiz(slov):
+    res = 1
     for k in slov:
         res *= slov[k]
     return(res)
     
 print('Сумма элементов = ', summ({5: 1, 2: 5, 1: 3, 3: 4, 6: 6}))
-print('Произведение элементов = ', proiz({5: 1, 2: 5, 1: 3, 3: 4, 6: 6}, 1, 0))
+print('Произведение элементов = ', proiz({5: 1, 2: 5, 1: 3, 3: 4, 6: 6}))
